64fb1c461810c740a37ca6e9243500278a0c215f
Jilberta <gvakh10@freeuni.edu.ge>
2014-10-02 14:28:25 +0400

    Camera Preview Class to see what you're capturing

---
+++ HappyVision/app/src/main/java/happyvision/jilberta/leavingstone/happyvision/CameraPreview.java
@@ -0,0 +1,66 @@
+package happyvision.jilberta.leavingstone.happyvision;
+
+import android.content.Context;
+import android.hardware.Camera;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+
+/**
+ * Created by Jay on 10/2/2014.
+ */
+public class CameraPreview extends SurfaceView implements SurfaceHolder.Callback {
+    private SurfaceHolder holder;
+    private Camera camera;
+
+    public CameraPreview(Context context, Camera camera) {
+        super(context);
+        this.camera = camera;
+        holder = getHolder();
+        holder.addCallback(this);
+        holder.setType(SurfaceHolder.SURFACE_TYPE_PUSH_BUFFERS);
+    }
+
+
+    @Override
+    public void surfaceCreated(SurfaceHolder holder) {
+        if (holder.getSurface() == null) {
+            return;
+        }
+        try {
+            camera.stopPreview();
+        } catch (Exception e){
+            e.printStackTrace();
+        }
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (Exception e) {
+            // Log.d(TAG, "Error starting camera preview: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+        try {
+            camera.setPreviewDisplay(holder);
+            camera.startPreview();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder holder) {
+
+    }
+}
+
+
+
+
+
+
+
